b8e9c8b8d5642c17470f35f413211bca0c4c74a1
vasiliy <gdciumfu@678.ru>
2026-01-30 20:03:58 +0500

    зменение табов

--- task.js
+++ task.js
@@ -5,3 +5,18 @@ let sum = a + b;
 let difference = a - b;
 console.log("Сумма:" + sum)
 console.log("Разность:" + difference)
+
+let title = "";
+if (title === "") {
+    console.log("Заголовок пустой");
+} else {console.log("Заголовок:" + title);}
+
+let tasks = 7;
+
+if (tasks === 0) {
+    console.log("Список пуст,можно отдыхать");
+} else if (tasks > 0 && tasks <= 3) {
+    console.log("Есть задачи для выполнения");
+}else if (tasks > 3) {
+    console.log("Список задачч переполнен");
+}
