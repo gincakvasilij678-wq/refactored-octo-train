8c1feda19dbeb182293ba476f066ce4acb2c1b64
vasiliy <gdciumfu@678.ru>
2026-02-27 19:57:49 +0500

    27.02

---
+++ main.js
@@ -0,0 +1,114 @@
+const input = document.querySelector(".form-add__input");
+const addButton = document.querySelector(".form-add__button");
+const container = document.querySelector(".tasks");
+
+const searchInput = document.querySelector(".toolbar__search");
+const footer = document.querySelector(".footer-controls");
+const sortSelect = document.querySelector(".toolbar__sort");
+
+function renderTask(task) {
+  /* container.innertaskL = "";
+
+  tasks.forEach((task) => { */
+  const item = document.createElement("div");
+  item.classList.add("task");
+
+  const content = document.createElement("div");
+  content.classList.add("task__content");
+
+  const title = document.createElement("div");
+  title.classList.add("task__title");
+  title.textContent = task.text;
+
+  const meta = document.createElement("div");
+  meta.classList.add("task__meta");
+  meta.textContent = task.date;
+
+  content.append(title, meta);
+  const actions = document.createElement("div");
+  actions.classList.add("task__actions");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("task__action", "task__action--edit");
+  deleteBtn.innerHTML = `<svg
+              class="task__icon"
+              viewBox="0 0 24 24"
+              width="14 "
+              height="14"
+              fill="none"
+              stroke="#cb6e6e"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round">
+              
+              <polyline points="3 6 5 6 21 6" />
+              <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
+              <path d="M10 11v6" />
+              <path d="M14 11v6" />
+              <path d="M9 6V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2" />
+            </svg>`;
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("task__action", "task__action--edit");
+  editBtn.title = "Редактировать";
+  editBtn.innerHTML = `
+    <svg class="task__icon" viewBox="0 0 24 24" fill="none" stroke="#6f64a3" stroke-width="2"
+      stroke-linecap="round" stroke-linejoin="round">
+      <path d="M12 20h9" />
+      <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" />
+    </svg>`;
+
+  deleteBtn.addEventListener("click", () => {
+    const index = tasks.indexOf(task);
+    tasks.splice(index, 1);
+
+    renderAll();
+
+    
+  });
+
+  actions.append(editBtn, deleteBtn);
+  item.append(content, actions);
+
+  if (task.done) {
+    item.classList.add("task--done");
+  }
+
+  item.addEventListener("click", (event) => {
+    console.log(event.target);
+    if (event.target.closest(".task__action")) return;
+    task.done = !task.done;
+    renderAll();
+  });
+
+  return item;
+  /* container.append(item);
+  }); */
+}
+
+// const task1 = renderTask({
+//   text: "Погулять с собакой",
+//   date: "Завтра 17:00",
+// });
+// const task2 = renderTask({
+//   text: "Прочитать книгу",
+//   date: "Завтра 17:00",
+// });
+
+// container.append(task1, task2)
+
+const tasks = [
+  { text: "Прочитать книгу", date: "Завтра 17:00", done: true },
+  { text: "Погулять с собакой", date: "Завтра 17:00", done: false },
+];
+
+function renderAll() {
+   container.innerHTML = ""; 
+console.log( document.querySelectorAll(".task"))
+ /*  document.querySelectorAll(".task").forEach(t => t.remove); */
+  tasks.forEach((task) => {
+    const card = renderTask(task);
+    footer.before(card);
+  });
+}
+
+renderAll();
